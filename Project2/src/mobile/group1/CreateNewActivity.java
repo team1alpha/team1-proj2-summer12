4dd68b477d6a0b2fd01f200479af00e72090dfbc
Steve Runion <gsrunion@gmail.com>
2012-07-11 21:35:31 -0400

    Adding project framework create by Craig. ~SR

---
+++ Project2/src/mobile/group1/CreateNewActivity.java
@@ -0,0 +1,27 @@
+package mobile.group1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class CreateNewActivity extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.create_new_account);
+	    // TODO Auto-generated method stub
+	}
+	
+	
+	
+	public void createAccountHandler(View v){
+		
+		//do what we need to to create and account and insert into the database
+		
+	}
+	
+	
+
+}
