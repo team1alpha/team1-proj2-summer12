96722d85cd4bac88833461b7fb6d150a6e3d432d
Steve Runion <gsrunion@gmail.com>
2012-07-14 01:14:18 -0400

    Got user read and write to/from database working. Need to implement updating already inserted users and preventing users from being added twice.

--- Project2/src/mobile/group1/DB/ScavengerDB.java
+++ Project2/src/mobile/group1/DB/ScavengerDB.java
@@ -2,6 +2,7 @@ package mobile.group1.DB;
 
 import java.util.HashMap;
 import java.util.Vector;
+import java.util.concurrent.Semaphore;
 
 import android.util.Log;
 
@@ -9,78 +10,30 @@ import com.mobdb.android.MobDB;
 import com.mobdb.android.GetRowData;
 import com.mobdb.android.MobDBResponseListener;
 
-public class ScavengerDB 
+public class ScavengerDB
 {
-	private final String APP_KEY    = "MIRoAA-5T3-uym202kOKKkKuIiZxZErELos-popgfD77YeatrtTsp6WoBmmM";
-	private final String TABLE_NAME = "user";
-	
-	public void addUser(User user)
-	{
-		
-	}
-	
-	public User getUser(String username)
-	{
-		
+	public static final String APP_KEY = "MIRoAA-5T3-uym202kOKKkKuIiZxZErELos-popgfD77YeatrtTsp6WoBmmM";
 
-		GetRowData getRowData = new GetRowData(TABLE_NAME);
-		getRowData.whereEqualsTo("test", username);
-		
-		MobDB.getInstance().execute(APP_KEY, getRowData, null, false, new MobDBResponseListener()
-		{
-			@Override public void mobDBSuccessResponse() {}
-			@Override public void mobDBResponse(String jsonObj) {}
-			@Override public void mobDBFileResponse(String fileName, byte[] fileData) {}
-
-
-			@Override
-			public void mobDBErrorResponse(Integer errValue, String errMsg) {
-				Log.i("", "error");
-			}
-
-			
-
-			@Override
-			public void mobDBResponse(Vector<HashMap<String, Object[]>> result) {
-				if(result.size() > 0)
-				{
-					HashMap<String, Object[]> item = result.get(0);
-					User user = new User((String)item.get("username")[0],
-									(String)item.get("password")[0],
-									(String)item.get("game")[0],
-									(String)item.get("score")[0],
-									(String)item.get("found")[0],
-									(String)item.get("p1")[0],
-									(String)item.get("p2")[0]);
-				}
-			}
-			
-		});
-		
-		
-		return null;
-	}
-	
 	public void addGame(Game game)
 	{
-		
+
 	}
-	
+
 	public Game getGame(String game)
 	{
 		return null;
 	}
-	
+
 	public Item getItem(int id)
 	{
 		return null;
 	}
-	
+
 	public void addItem(Item item)
 	{
-		
+
 	}
-	
+
 	public ScavengerDB()
 	{
 	}
--- Project2/src/mobile/group1/MainActivity.java
+++ Project2/src/mobile/group1/MainActivity.java
@@ -1,22 +1,51 @@
 package mobile.group1;
 
+import mobile.group1.DB.ScavengerDB;
+import mobile.group1.DB.User;
+import mobile.group1.DB.User.UserErrorListener;
+import mobile.group1.DB.User.UserListener;
 import android.app.Activity;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity 
+{
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
+        UserListener userListener = new UserListener()
+		{
+			@Override
+			public void onUserDBAction(User user)
+			{
+				Log.i("me", "user listener:" + user.toString());
+			}
+		};
+		
+		UserErrorListener userErrorListner = new UserErrorListener()
+		{
+			@Override
+			public void onUserDBError(Integer errValue, String errMsg)
+			{
+				Log.i("me", "user error listener:" + errMsg + ":" + String.valueOf(errValue));
+			}
+		};
+
+		User.RequestUser("a", userListener, userErrorListner);    
+		User.RequestUser("b", userListener, userErrorListner);    
+		User.RequestUser("c", userListener, userErrorListner);    
         
-        
-        
-        
+		User d = new User("d", "d", "d", "d", ";");
+		d.Save(userListener, userErrorListner);
+		
+		User.RequestUser("d", userListener, userErrorListner);    
         
         
     }
